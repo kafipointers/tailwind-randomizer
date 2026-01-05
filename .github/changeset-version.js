388351771b4c0f2e9b7c384e5707147cfc1a42c0
Samarth Goyal <samarth.goyal1999@gmail.com>
2026-01-05 17:13:55 +0530

    fix: changeset-verion file

--- .github/changeset-version.js
+++ .github/changeset-version.js
@@ -1,7 +1,7 @@
 // ORIGINALLY FROM CLOUDFLARE WRANGLER:
 // https://github.com/cloudflare/wrangler2/blob/main/.github/changeset-version.js
 
-import { execSync } from "child_process";
+import { exec } from "child_process";
 
 // This script is used by the `release.yml` workflow to update the version of the packages being released.
 // The standard step is only to run `changeset version` but this does not update the package-lock.json file.
@@ -10,3 +10,4 @@ import { execSync } from "child_process";
 // See https://github.com/changesets/changesets/issues/421.
 exec("npx changeset version");
 exec("npm install");
+
